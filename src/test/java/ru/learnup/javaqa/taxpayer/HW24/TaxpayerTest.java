2e55e9394eede1a7f1050c7dc3d6f3b57ac5ffbc
akasyuka <aleksandrkasyuk@gmail.com>
2021-11-10 15:01:32 +0500

    HW 24 attempt 1

--- src/test/java/ru/learnup/javaqa/taxpayer/HW24/TaxpayerTest.java
+++ src/test/java/ru/learnup/javaqa/taxpayer/HW24/TaxpayerTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 public class TaxpayerTest {
 
     Taxpayer boss = new Taxpayer(100000, 50000);
+
     @BeforeEach
     public void setup() {
         boss.getIncome();
@@ -17,54 +18,61 @@ public class TaxpayerTest {
         boss.incExpenses(8000);
 
     }
+
     @Test
-    public void incIncome(){
-        int actual  = boss.getIncome();
+    public void incIncome() {
+        int actual = boss.getIncome();
         int expected = 112000;
 
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
-    public void incExpenses(){
-        int actual  = boss.getExpenses();
+    public void incExpenses() {
+        int actual = boss.getExpenses();
         int expected = 58000;
 
         Assertions.assertEquals(expected, actual);
     }
+
     @ParameterizedTest
     @CsvSource({"-120,0", "0,0"})
-    public void incIncomeNull(int incIncome, int expectedTotal){
-        int actual  = boss.incIncome(incIncome);
+    public void incIncomeNull(int incIncome, int expectedTotal) {
+        int actual = boss.incIncome(incIncome);
         int expected = expectedTotal;
 
         Assertions.assertEquals(expected, actual);
     }
+
     @ParameterizedTest
     @CsvSource({"-150,0", "0,0"})
-    public void incExpensesNull(int incExpenses, int expectedTotal){
-        int actual  = boss.incExpenses(incExpenses);
+    public void incExpensesNull(int incExpenses, int expectedTotal) {
+        int actual = boss.incExpenses(incExpenses);
         int expected = expectedTotal;
 
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
-    public void taxIncome(){
-        int actual  = boss.taxIncome();
+    public void taxIncome() {
+        int actual = boss.taxIncome();
         int expected = 6720;
 
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
-    public void taxIncomeExpenses(){
-        int actual  = boss.taxIncomeExpenses();
+    public void taxIncomeExpenses() {
+        int actual = boss.taxIncomeExpenses();
         int expected = 8100;
 
         Assertions.assertEquals(expected, actual);
     }
+
     @Test
-    public void taxIncomeExpensesZero(){
+    public void taxIncomeExpensesZero() {
         Taxpayer badboss = new Taxpayer(100000, 120000);
-        int actual  = badboss.taxIncomeExpenses();
+        int actual = badboss.taxIncomeExpenses();
         int expected = 0;
 
         Assertions.assertEquals(expected, actual);
