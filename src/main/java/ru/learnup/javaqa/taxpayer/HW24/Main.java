3ea5219dbd32fa9104bbc9de5f5f396be5760bcb
akasyuka <aleksandrkasyuk@gmail.com>
2021-11-20 10:27:49 +0500

    25 attempt 1

--- src/main/java/ru/learnup/javaqa/taxpayer/HW24/Main.java
+++ src/main/java/ru/learnup/javaqa/taxpayer/HW24/Main.java
@@ -1,1 +1,16 @@
-//
+package ru.learnup.javaqa.taxpayer.HW24;
+
+public class Main {
+    public static void main(String[] args) {
+        StepsManager list = new StepsManager();
+
+        list.setDay(1);
+        System.out.println(list.add(1, 100));
+        System.out.println(list.add(1, 200));
+
+        list.setDay(2);
+        System.out.println(list.add(2, 50));
+        System.out.println(list.add(2, 400));
+
+    }
+}
